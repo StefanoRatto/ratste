7b2e116d452a630341786550dd0a8a53986a844f
stefanoratto <stefano.ratto@gmail.com>
2021-05-25 01:52:57 -0500

    Created common headers for server and client modules.

---
+++ ratste_server.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+""" 
+ratste's server component.
+
+ratste is a general purpose Remote Access Tool written in Python3.
+
+This program is free software: you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation, either version 3 of the License, or (at your option) any later
+version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with
+this program. If not, see <http://www.gnu.org/licenses/>.
+"""
+
+__author__ = "https://github.com/StefanoRatto/"
+__date__ = "2021/05/25"
+__deprecated__ = False
+__license__ = "GPLv3"
+__version__ = "1.0.0"
